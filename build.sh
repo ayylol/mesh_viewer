f24c02a44763f53b7fe5830464ff5f96020603a5
Bimgus <Bimgus.Oroz@pm.me>
2026-07-01 19:55:46 -0400

    Use flag variable for building with debug

--- build.sh
+++ build.sh
@@ -4,4 +4,6 @@ mkdir build 2>/dev/null
 glslc src/shader.vert -o build/vert.spv
 glslc src/shader.frag -o build/frag.spv
 
-odin build src -out:mesh_viewer -debug
+#ODIN_FLAGS="-debug"
+odin build src -out:mesh_viewer $ODIN_FLAGS
+
